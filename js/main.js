7d18f644a6db43573556539fddab25d0939b3c20
Francisco Nunes <nunes.francisco766@gmail.com>
2017-03-04 13:07:32 +0000

    Changed Version of Jquery to allow tap support

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function Init(options) {
     });
 
     var audio = $("#bolos")[0];
-    $(".sound-controller").on('mouseenter, tap', function() {
+    $(".sound-controller").on('mouseenter tap', function() {
         audio.play();
     });
 
