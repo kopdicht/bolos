f8c16a6158c93a3fbe3d84b0efb1909d0cb03bfa
Francisco Nunes <Francisco Nunes>
2017-03-04 12:34:38 +0000

    Project Added to GIT

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function Init(options) {
+
+    var date = new Date(options.date);
+    var now = new Date();
+    var diff = (date.getTime() / 1000) - (now.getTime() / 1000);
+
+    var clock = $(options.clockSelector).FlipClock(diff, {
+        clockFace: 'DailyCounter',
+        countdown: true,
+        language: 'pt'
+    });
+
+    var audio = $("#bolos")[0];
+    $(".sound-controller").mouseenter(function() {
+        audio.play();
+    });
+
+}
